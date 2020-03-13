a9d4b06c2106e345167acd3b7ac3a54f596dc187
DESKTOP-MIT9Q8B\Yaskevich <andrez.yaskevich@gmail.com>
2020-03-13 20:41:42 +0300

    Second Commit to Check Git using WebSTorm

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const NavigationBar = () => {
     return (
-    <nav className="nav">
-    <div><a>Photo + My Name</a></div>
-    <div><a>News Feed</a></div>
-    <div><a>Messages</a></div>
-    <div><a>Groups</a></div>
-    <div><a>Music</a></div>
-    <div><a>My Pages</a></div>
-    </nav>);
+        <nav className="nav">
+            <div><a>Photo + My Name</a></div>
+            <div><a>News Feed</a></div>
+            <div><a>Messages</a></div>
+            <div><a>Groups</a></div>
+            <div><a>Music</a></div>
+            <div><a>My Pages</a></div>
+        </nav>);
 
 }
 
